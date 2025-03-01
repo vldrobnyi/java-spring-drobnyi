849377090764ea52265f319f191fea8e724d259f
vldrobnyi <drobnyivladyslav@gmail.com>
2025-03-01 14:42:39 +0200

    Initial commit

---
+++ src/main/java/edu/ntudp/sau/spring_java/SpringJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.ntudp.sau.spring_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ntudp/sau/spring_java/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntudp.sau.spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
