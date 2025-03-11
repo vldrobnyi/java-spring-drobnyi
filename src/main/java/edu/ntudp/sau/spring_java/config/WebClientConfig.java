bb3be52b02cc505ce741a6fd9a4c7ef33fe182d7
vldrobnyi <drobnyivladyslav@gmail.com>
2025-03-11 16:02:48 +0200

    Small change

--- src/main/java/edu/ntudp/sau/spring_java/config/WebClientConfig.java
+++ src/main/java/edu/ntudp/sau/spring_java/config/WebClientConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class CurrencyWebClientConfig {
+public class WebClientConfig {
 
     @Bean
     public WebClient currencyWebClient(WebClient.Builder builder) {
